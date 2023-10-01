3dd0d36df553419c771d0948966e44195de138d9
Kashinath Kolekar <125242548+kashikolekar@users.noreply.github.com>
2023-10-01 16:48:45 +0530

    Add files via upload
    SpringJDBC Connection Using XML

---
+++ SpringJDBC/src/main/java/com/Spring/JDBC/App.java
@@ -0,0 +1,48 @@
+package com.Spring.JDBC;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.Spring.JDBC.deo.StudentSDao;
+import com.Spring.JDBC.deo.studentDaoImple;
+import com.Spring.JDBC.entity.Student;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static  void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //Spring jdbc
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/Spring/JDBC/Config.xml");
+    
+  StudentSDao stuDao =     context.getBean("bean3",StudentSDao.class);
+  
+  //Student student = new Student(666,"Sharavani","shirala");
+ /* Student student = new Student();
+  student.setId(100);
+  student.setCity("Shirala");
+  student.setName("Ajit");
+     int res = stuDao.change(student);
+  
+ */
+  
+ // int res = stuDao.delete(10);
+  
+  //Student student = stuDao.getStudent(666);
+   List<Student> student = stuDao.getAll_Data();
+  
+           //   int res=  stuDao.insert(student);   
+           for(Student s:student)
+           {
+        	   System.out.println(s);
+           }
+    }
+}
